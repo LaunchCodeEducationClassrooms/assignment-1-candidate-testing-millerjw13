d6eeea734d0fdb34802a24dae02416ca2537133d
millerj13 <oldno70000@yahoo.com>
2021-02-21 20:34:32 +0000

    v10 fixing code

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,9 +8,9 @@ let candidateName = ''
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = '';
-let questions = ["Who was the first American woman in space?", "True or false: 5000 meters = 5 kilometers.", "(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
-let correctAnswers = [ "Sally Ride", "True", "40", "Trajectory", "3"];
-let candidateAnswers = '';
+let questions = ["Who was the first American woman in space?", "True or false: 5000 meters = 5 kilometers.", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
+let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 let candidateAnswersArray = [];
 let correctAnswerCnt = 0;
 
@@ -31,7 +31,7 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
   for (let i = 0; i < questions.length; i++) {
-    console.log(` ${candidateAnswersArray[i]}` + ' ' + `${correctAnswers[i]}`)
+    console.log(`${candidateAnswersArray[i]}` + ' ' + `${correctAnswers[i]}`)
 }
 
 console.log("\nCandidate Name: " + candidateName)

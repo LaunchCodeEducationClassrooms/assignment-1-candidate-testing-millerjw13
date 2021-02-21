dd577a4256f8b3663282a0ece50f48b92114992f
millerj13 <oldno70000@yahoo.com>
2021-02-21 21:40:20 +0000

    v13 still undefined?

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,8 +8,8 @@ let candidateName = ''
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = '';
-let questions = ['Who was the first American woman in space?', 'True or false: 5000 meters = 5 kilometers.', '(5 + 3)/2 * 10 = ?', "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", 'What is the minimum crew size for the ISS?'];
-let correctAnswers = [ 'Sally Ride', 'True', '40', 'Trajectory', '3' ];
+let questions = ["Who was the first American woman in space?", "True or false: 5000 meters = 5 kilometers.", "(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
+let correctAnswers = [ "Sally Ride", "True", "40", "Trajectory", "3"];
 let candidateAnswers = '';
 let candidateAnswersArray = [];
 let correctAnswerCnt = 0;
@@ -34,11 +34,15 @@ function gradeQuiz(candidateAnswers) {
     console.log(` ${candidateAnswersArray[i]}` + ' ' + `${correctAnswers[i]}`)
 }
 
+      correctAnswers.toUpperCase
+      candidateAnswersArray.toUpperCase
+
 console.log("\nCandidate Name: " + candidateName)
      for (let i = 0; i < questions.length; i++) {
        console.log(`${i+1}) ${questions[i]}\nYour Answer: ${candidateAnswersArray[i]}\nCorrect Answer: ${correctAnswers[i]}\n`)
         
-       if (correctAnswers[i].toUpperCase() == candidateAnswersArray[i].toUpperCase()) {
+
+       if (correctAnswers[i] == candidateAnswersArray[i]) {
         correctAnswerCnt +=1} 
   //console.log(correctAnswerCnt)
 }

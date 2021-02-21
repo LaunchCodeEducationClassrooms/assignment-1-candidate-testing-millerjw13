061e78cf4c07711ad803727cc329e02c955fe460
millerj13 <oldno70000@yahoo.com>
2021-02-21 19:16:37 +0000

    V2

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,11 +8,11 @@ let candidateName = ''
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = '';
-let questions = ["Who was the first American woman in space?", "True or false: 5000 meters = 5 kilometers.", "(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
-let correctAnswers = [ "Sally Ride", "True", "40", "Trajectory", "3"];
-let candidateAnswers = ''
-let candidateAnswersArray = []
-let correctAnswerCnt = 0
+let questions = [ "Who was the first American woman in space? ", "True or false: 5000 meters = 5 kilometers. ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? " ];
+let correctAnswers = [ "Sally Ride", "True", "40", "Trajectory", "3" ];
+let candidateAnswers = ('');
+let candidateAnswersArray = [];
+let correctAnswerCnt = 0;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -38,7 +38,7 @@ console.log("\nCandidate Name: " + candidateName)
      for (let i = 0; i < questions.length; i++) {
        console.log(`${i+1}) ${questions[i]}\nYour Answer: ${candidateAnswersArray[i]}\nCorrect Answer: ${correctAnswers[i]}\n`)
         
-       if (correctAnswers[i].toUpperCase() == candidateAnswersArray[i].toUpperCase()) {
+       if (correctAnswers[i].toLowerCase() == candidateAnswersArray[i].toLowerCase()) {
         correctAnswerCnt +=1} 
   //console.log(correctAnswerCnt)
 }
